a31a1219dc6ef55a9c803e9da1816bff03896362
Derek Hanciles <drhanciles@icloud.com>
2018-07-03 16:59:15 -0600

    Add check range function

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ var maxGuess = document.querySelector('.max-guess');
 var guessButton = document.querySelector('.guess-button');
 var clearButton = document.querySelector('.clear-button'); 
 var resetButton = document.querySelector('.reset-button'); 
+var rangeButton = document.querySelector('.submit-range')
 var displayGuess = document.querySelector('.display-guess');
 var gameIntro = document.querySelector('.game-intro');
 var gameFeedBack = document.querySelector('.game-feedback');
+
+
+var guessCount = 1; 
 var minValue = 1; 
 var maxValue = 100; 
 var randomNumber = getRandomNumber(1,100); 
 console.log(randomNumber); 
-var guessCount = 1; 
 
 // Event Listeners 
 userGuess.addEventListener('keydown', enableButtons)
 guessButton.addEventListener('click', playGame);
 clearButton.addEventListener('click', clearInput);
 resetButton.addEventListener('click', resetGame);
+rangeButton.addEventListener('click', checkRange); 
+
 
 // Functions 
 function getRandomNumber(min, max) {
@@ -35,7 +40,6 @@ function playGame(event) {
   event.preventDefault(); 
   getRandomNumber();
   displayGuess.innerText = userGuess.value;
-  // var guessInput = parseInt(userGuess.value)
   guessButton.disabled = true; 
   clearButton.disabled = true; 
   checkGuess(parseInt(userGuess.value));
@@ -66,6 +70,13 @@ function enableButtons(event) {
   guessButton.disabled = false; 
   clearButton.disabled = false; 
   resetButton.disabled = false; 
+}
+
+function checkRange() {
+  event.preventDefault(); 
+  randomNumber = getRandomNumber(parseInt(minGuess.value), parseInt(maxGuess.value)); 
+  console.log(randomNumber); 
+  console.log(minGuess.value, maxGuess.value); 
 }
 
 
